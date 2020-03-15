2c57732f0ba176562e1759f171b8905dcf06b07d
chenmeng <1487768100@qq.com>
2020-03-15 19:35:52 +0800

    初始化前端页面，antd和TS配置

---
+++ movie/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {Button} from 'antd'
+ 
+function App() {
+  return (
+    <div >
+      <Button type="danger">hi</Button>
+    </div>
+  );
+}
+
+export default App;
